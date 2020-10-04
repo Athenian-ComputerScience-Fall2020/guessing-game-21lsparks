d6f33495f3cd4f462bdc81dcafd14a422178bcc1
21lsparks <21lsparks@athenian.org>
2020-10-04 19:40:05 +0000

    in progress

--- game1.py
+++ game1.py
@@ -9,3 +9,21 @@ Use this file to write an "open" version of the game (no test code or defined fo
 
 '''
 # Collaborators: 
+
+
+import random
+target = random.randint(0,10)
+print(target)
+print("Hi! Welcome to the Guessing Game. You have five guesses.")
+for guesses in range(1, 6):
+    guess = input("What is your guess #" + str(guesses) + "? ")
+    if guess == "done":
+        exit()
+    guess = int(guess)
+    if guess > target:
+        print("Sorry! " +  str(guess) + " is too big!")
+    elif guess < target:
+        print("Sorry! " + str(guess) " is too small")
+    else:
+        print("Great job! " + str(guess) + " is correct!")
+        exit()
